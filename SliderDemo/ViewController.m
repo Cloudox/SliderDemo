4faa57e968ac1d1f891e1a4f23ad9398704bad89
Cloudox <cloudox@foxmail.com>
2016-03-24 22:49:57 +0800

    显示滑块及label，但无法滑动

--- SliderDemo/ViewController.m
+++ SliderDemo/ViewController.m
@@ -10,13 +10,38 @@
 
 @interface ViewController ()
 
+//设备的宽高
+#define SCREENWIDTH       [UIScreen mainScreen].bounds.size.width
+#define SCREENHEIGHT      [UIScreen mainScreen].bounds.size.height
+
+@property (nonatomic, strong) UILabel *valueLabel;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake((SCREENWIDTH - 300) / 2, 200, 300, 0)];
+    slider.minimumValue = 9;
+    slider.maximumValue = 11;
+    [slider setValue:10 animated:YES];
+    slider.continuous = YES;
+    slider.userInteractionEnabled = YES;
+    [slider addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
+    
+    [self.view addSubview:slider];
+    
+    self.valueLabel = [[UILabel alloc] initWithFrame:CGRectMake((SCREENWIDTH - 100) / 2, slider.frame.origin.y + 30, 100, 20)];
+    self.valueLabel.textAlignment = NSTextAlignmentCenter;
+    self.valueLabel.text = [NSString stringWithFormat:@"%.1f", slider.value];
+    [self.view addSubview:self.valueLabel];
+}
+
+- (void)sliderValueChanged:(id)sender {
+    UISlider *slider = (UISlider *)sender;
+    self.valueLabel.text = [NSString stringWithFormat:@"%.1f", slider.value];
 }
 
 - (void)didReceiveMemoryWarning {
