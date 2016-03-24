4ccea674ab2e19785fea45ef71ae593d0a5a27ea
Cloudox <cloudox@foxmail.com>
2016-03-24 22:16:27 +0800

    Initial Commit

---
+++ SliderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SliderDemo
+//
+//  Created by Cloudox on 16/3/24.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SliderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SliderDemo
+//
+//  Created by Cloudox on 16/3/24.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
